bc756e83ddf928ad79585d920329aa89d6ab7a44
Walter Sosa <sosa.tizon@gmail.com>
2017-03-19 20:28:50 -0300

    Cambio de co por async await nativo

--- rutas/contribuyentes.js
+++ rutas/contribuyentes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const Bd = require('../contribuyentes')
 const config = require('../config')
 const formData = require('express-form-data')
-const co = require('co')
 
 // parsing data with connect-multiparty. Result set on req.body and req.files
 router.use(formData.parse());
@@ -32,14 +31,14 @@ router.get('/listar', async function  (req, res) {
   }
   })
 
-router.get('/nuevo', co.wrap(function * (req, res) {
+router.get('/nuevo',  async function  (req, res) {
   let db = new Bd()
-  let ciclos = yield db.getCiclos()
+  let ciclos = await db.getCiclos()
   db.disconnect()
   res.render('contribuyentes-nuevo', {ciclos: ciclos})
-}))
+})
 
-router.get('/prueba', co.wrap(function * (req, res) {
+router.get('/prueba', async function  (req, res) {
   let db = new Bd()
   let fecha = new Date().toJSON().slice(0,10)
   let contris = []
@@ -49,41 +48,41 @@ router.get('/prueba', co.wrap(function * (req, res) {
 
   console.log(contris[0])
 
-  yield db.generarCli(contris)
+ await db.generarCli(contris)
   //res.send('Mes generado con éxito')
   res.end()
-}))
+})
 
-router.get('/editar/:id', co.wrap(function * (req, res) {
+router.get('/editar/:id', async function  (req, res) {
   let db = new Bd()
   let id = req.params.id
 
-  let contri = (yield db.getContribuyente(id))[0]
+  let contri = (await db.getContribuyente(id))[0]
  // let contri = contris[0]
 
-  let ciclos = yield db.getCiclos()
+  let ciclos = await db.getCiclos()
   db.disconnect()
   res.render('contribuyentes-edit', {contri: contri, ciclos: ciclos})
 
-}))
+})
 
 router.get('/generar', function(req, res) {
   res.render('cuotas-generar')
 })
 
-router.post('/generar', co.wrap(function * (req, res) {
+router.post('/generar', async function  (req, res) {
   let db = new Bd()
   let mes = req.body.mes
   let anio = req.body.anio
-  let contris = yield db.getContribuyentes(' WHERE estado = 1')
+  let contris = await db.getContribuyentes(' WHERE estado = 1')
   let fecha = new Date().toJSON().slice(0,10)
   let filas = []
   contris.forEach(function(el) {
     filas.push([el.id_contribuyente, mes, anio, 0, fecha])
   })
-  yield db.generarCuotas(filas)
+  await db.generarCuotas(filas)
   res.send('Mes generado con éxito')
 
-}))
+})
 
 module.exports = router
