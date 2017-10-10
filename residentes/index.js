d9ee51bf660e80fa493067ca1099230c3f241e16
Walter Sosa <sosa.tizon@gmail.com>
2017-10-10 00:33:41 -0300

    Avance en residentes

--- residentes/index.js
+++ residentes/index.js
@@ -1,4 +1,4 @@
-const co = require('co')
+
 const mysql = require('promise-mysql')
 let config = require('../config')
 
@@ -8,7 +8,7 @@ class Residentes {
     this.connect()
   }
 
-  connect() {
+  cconnect() {
 
     let options = {
       host: config.host,
@@ -20,68 +20,64 @@ class Residentes {
     this.con = mysql.createConnection(options)
 
     let connection = this.con
-    let setup = co.wrap(function * () {
-      let  conn = yield connection
-      return conn
-    })
-    return Promise.resolve(setup())
+
+    return Promise.resolve(connection)
   }
 
-  disconnect() {
+  async disconnect() {
     let connection = this.con
-    let setup = co.wrap(function * () {
-      let conn = yield connection
-      conn.destroy()
-      return conn
-    })
-    return Promise.resolve(setup())
+    let conn = await connection
+    conn.destroy()
+    return conn
   }
 
-  saveResidente(residente) {
+  async saveResidente(residente) {
 
     let connection = this.con
-    let task = co.wrap(function * () {
-      let conn = yield connection
-      let sql = 'INSERT INTO cuotas (id_residente, nombre, apellido, documento, fecha_nacimiento, mutualista, cuidados, fecha_ingreso) VALUES ?'
-      let result = yield conn.query(sql, residente)
-      if(!result) {
-        return Promise.reject(new Error('Ocurrio un error'))
-      }
-      return Promise.resolve(result)
-    })
-    return Promise.resolve(task())
-
+    let conn = await connection
+    let sql = 'INSERT INTO cuotas (id_residente, nombre, apellido, documento, fecha_nacimiento, mutualista, cuidados, fecha_ingreso) VALUES ?'
+    let result = await conn.query(sql, residente)
+    if(!result) {
+      return Promise.reject(new Error('Ocurrio un error'))
+    }
+    return Promise.resolve(result)
   }
 
   /*delResidente(id) {
 
   }*/
 
-  getResidentes() {
+  async getResidentes() {
     let connection = this.con
-    let task = co.wrap(function * () {
-      let conn = yield connection
-      let lista = yield conn.query('SELECT * FROM residentes WHERE estado = 1')
-      if(!lista) {
-        return Promise.reject(new Error('Ocurrio un error'))
-      }
-      return Promise.resolve(lista)
-    })
-    return Promise.resolve(task())
+
+    let conn = await connection
+    let lista = await conn.query('SELECT * FROM residentes WHERE estado = 1')
+    if(!lista) {
+      return Promise.reject(new Error('Ocurrio un error'))
+    }
+    return Promise.resolve(lista)
 
   }
 
-  getResidente(id) {
+  async delResidente(id) {
     let connection = this.con
-    let task = co.wrap(function * () {
-      let conn = yield connection
-      let residente = yield conn.query(`SELECT * FROM residentes WHERE estado = 1 AND id_residente = ${id}`)
-      if(!residente) {
-        return Promise.reject(new Error('Ocurrio un error'))
-      }
-      return Promise.resolve(residente)
-    })
-    return Promise.resolve(task())
+
+    let conn = await connection
+    let result = await conn.query(`DELETE FROM residentes WHERE id_residente=${id}`)
+    if(!result) {
+      return Promise.reject(new Error('Ocurrio un error'))
+    }
+    return Promise.resolve(result)
+  }
+
+  async getResidente(id) {
+    let connection = this.con
+    let conn = await connection
+    let residente = await conn.query(`SELECT * FROM residentes WHERE estado = 1 AND id_residente = ${id}`)
+    if(!residente) {
+      return Promise.reject(new Error('Ocurrio un error'))
+    }
+    return Promise.resolve(residente)
   }
 
 }
